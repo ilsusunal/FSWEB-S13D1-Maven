41c7d2cc1b4b22162820bcc4916785b3905aab64
ilsu-sunal <sunalilsu@gmail.com>
2024-03-18 18:03:42 +0300

    Problem 1-2-3

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,15 +6,42 @@ public class Main {
     }
 
     public static boolean shouldWakeUp(boolean isBarking, int clock) {
-        
+        //clock>=8 && clock<=20 -> isBarking == true
+        //clock<8 && clock>20 -> isBarking == true
+        if (clock < 0 || clock >23) {
+            return false;
+        }
+        if (isBarking){
+            if (clock < 8 || clock > 20) {
+                return true;
+            }
+        }
+        return false;
     }
 
     public static boolean hasTeen(int firstAge, int secondAge, int thirdAge) {
-       
+        // int <19 && int>13 -> true
+       if (firstAge < 19 && firstAge >13) {
+           return true;
+       }
+        if (secondAge < 19 && secondAge >13) {
+            return true;
+        }
+        if (thirdAge < 19 && thirdAge >13) {
+            return true;
+        }
+        return false;
     }
 
     public static boolean isCatPlaying(boolean isSummer, int temp) {
-     
+        // temp<35 && temp>25 -> true
+        // isSummer == true temp<45 -> true
+        if (isSummer) {
+            if (temp < 45 && temp > 25) {
+                return true;
+            }
+        }
+    return false;
     }
 
     public static double area(double width, double height) {
